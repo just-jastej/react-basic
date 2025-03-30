dba382144a43940d303742f35f946d7f9ea84f74
just-jastej <jastejsingh.chandhok@gmail.com>
2025-03-30 12:15:45 +0530

    context api discuss

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements,Route } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github from './components/Github/Github.jsx'  
+import Github, { githubInfoLoader }  from './components/Github/Github.jsx'  
 
 // const router = createBrowserRouter([
 //     {
@@ -39,7 +39,7 @@ const router = createBrowserRouter(
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />}  />
       <Route 
-      // loader={githubInfoLoader}
+      loader={githubInfoLoader}
       path='github' 
       element={<Github />} 
       />
